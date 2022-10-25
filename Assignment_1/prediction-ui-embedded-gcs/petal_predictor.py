3535f90565981d4c6996b4e0315cbacfdbff76e3
Marty1913 <38720459+Marty1913@users.noreply.github.com>
2022-10-25 13:35:10 +0200

    Update petal_predictor.py

--- Assignment_1/prediction-ui-embedded-gcs/petal_predictor.py
+++ Assignment_1/prediction-ui-embedded-gcs/petal_predictor.py
@@ -1,17 +1,15 @@
 import json
 
 import pandas as pd
-from flask import jsonify
 import pickle
-import os
 
 class PetalPredictor:
     def __init__(self):
         self.model = None
 
     def predict_single_record(self, df):
-        model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
         if self.model is None:
-            self.model = pickle.load(open(model_name, 'rb'))
+            self.model = pickle.load(open("model.sav", 'rb'))
         y_pred = self.model.predict(df)
+        print(y_pred[0])
         return y_pred[0]
