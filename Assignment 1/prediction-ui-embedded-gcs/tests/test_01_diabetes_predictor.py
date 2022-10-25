449ffc5848fa563c78ca39f65c1d93e475440ac7
Marty1913 <38720459+Marty1913@users.noreply.github.com>
2022-10-25 11:20:19 +0200

    Update test_01_diabetes_predictor.py

--- Assignment 1/prediction-ui-embedded-gcs/tests/test_01_diabetes_predictor.py
+++ Assignment 1/prediction-ui-embedded-gcs/tests/test_01_diabetes_predictor.py
@@ -4,20 +4,20 @@ import pytest
 import pandas as pd
 
 # content of test_class.py
-import diabetes_predictor
+import petal_predictor
 
 
-class TestDiabetesPredictor:
+class TestPetalPredictor:
 
     @pytest.fixture(scope="session", autouse=True)
     def execute_before_any_test(self):
-        os.environ["MODEL_NAME"] = "testResources/model.h5"
+        os.environ["MODEL_NAME"] = "testResources/model.sav"
 
     # your setup code goes here, executed ahead of first test
     def test_predict_single_record(self):
         with open('testResources/prediction_request.json') as json_file:
             data = pd.read_json(json_file)
-        dp = diabetes_predictor.DiabetesPredictor()
-        status = dp.predict_single_record(data)
+        pp = petal_predictor.PetalPredictor()
+        status = pp.predict_single_record(data)
         assert bool(status[0]) is not None
         assert bool(status[0]) is False
