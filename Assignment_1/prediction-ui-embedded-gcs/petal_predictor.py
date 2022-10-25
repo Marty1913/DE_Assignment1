ce7c52c39002881c344cf658a3255d4aad6321f0
Marty1913 <38720459+Marty1913@users.noreply.github.com>
2022-10-25 14:59:22 +0200

    Update petal_predictor.py

--- Assignment_1/prediction-ui-embedded-gcs/petal_predictor.py
+++ Assignment_1/prediction-ui-embedded-gcs/petal_predictor.py
@@ -11,5 +11,5 @@ class PetalPredictor:
         if self.model is None:
             self.model = pickle.load(open("model.sav", 'rb'))
         y_pred = self.model.predict(df)
-        print(y_pred[0])
-        return y_pred[0]
+        print(y_pred)
+        return y_pred
