1914a3a36dc5049d90aaac5bf0bef3eedbf2fc4d
Marty1913 <38720459+Marty1913@users.noreply.github.com>
2022-10-25 13:08:04 +0200

    Update app.py

--- Assignment_1/prediction-ui-embedded-gcs/app.py
+++ Assignment_1/prediction-ui-embedded-gcs/app.py
@@ -37,4 +37,4 @@ def check_petal():
 
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)), host='0.0.0.0', debug=True)
