1bb9bbc199dbaf438497ea54dadd712b6724b4d5
Marty1913 <38720459+Marty1913@users.noreply.github.com>
2022-10-25 13:28:34 +0200

    Update test_01_petal_predictor.py

--- Assignment_1/prediction-ui-embedded-gcs/tests/test_01_petal_predictor.py
+++ Assignment_1/prediction-ui-embedded-gcs/tests/test_01_petal_predictor.py
@@ -8,12 +8,6 @@ import petal_predictor
 
 
 class TestPetalPredictor:
-
-    @pytest.fixture(scope="session", autouse=True)
-    def execute_before_any_test(self):
-        os.environ["MODEL_NAME"] = "testResources/model.sav"
-
-    # your setup code goes here, executed ahead of first test
     def test_predict_single_record(self):
         with open('testResources/prediction_request.json') as json_file:
             data = pd.read_json(json_file)
